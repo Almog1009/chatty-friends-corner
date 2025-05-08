2929f1bef54136f0db14ff5a98044e13dd8aee11
shaharmayzel <lilcodemonkey@gmail.com>
2025-05-08 20:49:50 +0300

    change user interface and supporting functions

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,13 +4,23 @@ export interface User {
   id: string;
   name: string;
   pronouns: string;
-  score: number;
-  summary: string;
-  friends: string[];
+  mySupporters: string[];
+  supporties: string[];
+  mindTributes: MindTribute[] | null;
+}
+
+interface MindTribute {type: MindTributeType, score: number, summary: string};
+
+enum MindTributeType {
+  "anxiety",
+  "sadness",
+  "loneliness",
+  "fear",
+  "anger"
 }
 
 class UserService {
-  private users: User[] = userData.users;
+  private users: User[] = userData.users as unknown as User[];
 
   getAllUsers(): User[] {
     return this.users;
@@ -20,53 +30,82 @@ class UserService {
     return this.users.find((user) => user.id === id);
   }
 
-  getFriends(userId: string): User[] {
+  getSupporters(userId: string): User[] {
     const user = this.getUserById(userId);
     if (!user) return [];
-
-    return user.friends
-      .map((friendId) => this.getUserById(friendId))
+    return user.mySupporters
+      .map((supporterId) => this.getUserById(supporterId
+      ))
       .filter((user): user is User => user !== undefined);
   }
 
-  addFriend(userId: string, friendId: string): boolean {
+  getSupporties(userId: string): User[] {
     const user = this.getUserById(userId);
-    const friend = this.getUserById(friendId);
-
-    if (!user || !friend) return false;
-    if (user.friends.includes(friendId)) return false;
-
-    user.friends.push(friendId);
-    friend.friends.push(userId);
-    return true;
+    if (!user) return [];
+    return user.supporties
+      .map((supportyId) => this.getUserById(supportyId))
+      .filter((user): user is User => user !== undefined);
   }
 
-  removeFriend(userId: string, friendId: string): boolean {
+
+  getMindTributes(userId: string): MindTribute[] {
     const user = this.getUserById(userId);
-    const friend = this.getUserById(friendId);
-
-    if (!user || !friend) return false;
-
-    user.friends = user.friends.filter((id) => id !== friendId);
-    friend.friends = friend.friends.filter((id) => id !== userId);
-    return true;
+    if (!user) return [];
+    return user.mindTributes || [];
   }
 
-  updateUserScore(userId: string, newScore: number): boolean {
-    const user = this.getUserById(userId);
-    if (!user) return false;
+  
 
-    user.score = newScore;
-    return true;
-  }
 
-  updateUserSummary(userId: string, newSummary: string): boolean {
-    const user = this.getUserById(userId);
-    if (!user) return false;
 
-    user.summary = newSummary;
-    return true;
-  }
+
+  // getFriends(userId: string): User[] {
+  //   const user = this.getUserById(userId);
+  //   if (!user) return [];
+
+  //   return user.friends
+  //     .map((friendId) => this.getUserById(friendId))
+  //     .filter((user): user is User => user !== undefined);
+  // }
+
+  // addFriend(userId: string, friendId: string): boolean {
+  //   const user = this.getUserById(userId);
+  //   const friend = this.getUserById(friendId);
+
+  //   if (!user || !friend) return false;
+  //   if (user.friends.includes(friendId)) return false;
+
+  //   user.friends.push(friendId);
+  //   friend.friends.push(userId);
+  //   return true;
+  // }
+
+  // removeFriend(userId: string, friendId: string): boolean {
+  //   const user = this.getUserById(userId);
+  //   const friend = this.getUserById(friendId);
+
+  //   if (!user || !friend) return false;
+
+  //   user.friends = user.friends.filter((id) => id !== friendId);
+  //   friend.friends = friend.friends.filter((id) => id !== userId);
+  //   return true;
+  // }
+
+  // updateUserScore(userId: string, newScore: number): boolean {
+  //   const user = this.getUserById(userId);
+  //   if (!user) return false;
+
+  //   user.score = newScore;
+  //   return true;
+  // }
+
+  // updateUserSummary(userId: string, newSummary: string): boolean {
+  //   const user = this.getUserById(userId);
+  //   if (!user) return false;
+
+  //   user.summary = newSummary;
+  //   return true;
+  // }
 }
 
 export const userService = new UserService();
